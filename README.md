4787643dc7ffeeed36222495a4fb9f596a0a24bf
ZachWagner1 <zwag14@yahoo.com>
2020-11-01 09:02:40 -0600

    fix

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
   ![MIT license badge](https://img.shields.io/badge/license-MIT-green)
 
 ![Google Books Search](./assets/images/screenshot.png)
-
+[Visit Site](https://warm-river-89849.herokuapp.com/)
 
 
 ## Description
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,17 +5,18 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-        // if .user has been added to req by authMiddleware, user is validated
-        if (context.user) {
-          const userData = await User.findOne({_id: context.user._id})
-            .select('-__v -password')
-  
-          return userData;
-        }
-  
-        throw new AuthenticationError('Not logged in');
-      },
+      // if .user has been added to req by authMiddleware, user is validated
+      if (context.user) {
+        const userData = await User.findOne({_id: context.user._id})
+          .select('-__v -password')
+          // not sure what to populate...check front end.  need savedBooks array from User to map over for component
+        return userData;
+      }
 
+      throw new AuthenticationError('Not logged in');
+    }, 
+
+    // TESTING
     users: async () => {
       return User.find()
     },
@@ -34,32 +35,31 @@ const resolvers = {
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials')
       }
-
-      // if password is correct, sign token and return to user
+      // if password is correct, sign token and return token and user
       const token = signToken(user);
       return { token, user }
     },
 
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      // sign token and return object that combines token with user's data
+      // sign a token and return an object that combines the token with the user's data
       const token = signToken(user);
 
       return { token, user }
     },
 
     saveBook: async (parent, { book }, context) => {
-
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           {_id: context.user._id},
           { $addToSet: { savedBooks: book}},
+          // do i really need to run validators here?
           { new: true, runValidators: true }
           // { new: true }
         )
+        // console.log(updatedUser);
         return updatedUser;
       }
-
       throw new AuthenticationError('You need to be loggedin to save a book');
     }, 
 
@@ -68,16 +68,15 @@ const resolvers = {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           {_id: context.user._id},
+          // not sure...
           { $pull: { savedBooks: { bookId: bookId } } },
           { new: true }
-        )
-
+        )   
         return updatedUser;
       }
-
       throw new AuthenticationError('You need to be logged in')
+    }
   }
-} 
 }
 
 module.exports = resolvers;
